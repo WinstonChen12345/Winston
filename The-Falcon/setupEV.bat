4a88b681314afed4a27c41488349356e6387aebb
WinstonChen12345 <yunsing123@gmail.com>
2025-04-22 13:46:32 +0800

    The Falcon

---
+++ The-Falcon/setupEV.bat
@@ -0,0 +1,27 @@
+::# ----------------------------------------------------------------------------------------
+::# Environmental Variables for Lazytrade System
+::# ----------------------------------------------------------------------------------------
+::# User Environmental Variables
+::# Check in: Computer Properties -> Advanced System Settings -> Environmental Variables -> User Variables
+
+::# Customize path to the user folder where all relevantrepositories should be located
+setx PATH_DSS_Repo "%USERPROFILE%\Desktop\showcase"
+
+::# Customize path to the user folders with Trading Platforms
+setx PATH_T1 "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\D248B9EB8BEF6A51D388375D3000ECEF\MQL4\Files"
+setx PATH_T1_E "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\D248B9EB8BEF6A51D388375D3000ECEF\MQL4\Experts"
+setx PATH_T1_I "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\D248B9EB8BEF6A51D388375D3000ECEF\MQL4\Include"
+
+setx PATH_T2 "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\E5C9D9C94B1652EEDD97AB31AC1AF67A\MQL4\Files"
+setx PATH_T2_E "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\E5C9D9C94B1652EEDD97AB31AC1AF67A\MQL4\Experts"
+setx PATH_T2_I "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\E5C9D9C94B1652EEDD97AB31AC1AF67A\MQL4\Include"
+
+setx PATH_T3 "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\96DED2BEA4EAFD4A9030F1AD80A30352\MQL4\Files"
+setx PATH_T3_E "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\96DED2BEA4EAFD4A9030F1AD80A30352\MQL4\Experts"
+setx PATH_T3_I "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\96DED2BEA4EAFD4A9030F1AD80A30352\MQL4\Include"
+
+setx PATH_T4 "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\9D7392573B48756D014806227A808DE9\MQL4\Files"
+setx PATH_T4_E "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\9D7392573B48756D014806227A808DE9\MQL4\Experts"
+setx PATH_T4_I "C:\Users\yunsi\AppData\Roaming\MetaQuotes\Terminal\9D7392573B48756D014806227A808DE9\MQL4\Include"
+
+
